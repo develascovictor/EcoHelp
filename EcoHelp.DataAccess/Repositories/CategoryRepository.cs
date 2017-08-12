aa419b829c45ff7b57d7c2f7df721c73d7fdc937
Victor De Velasco <develasco.victor@gmail.com>
2017-08-12 15:57:08 -0700

    Added first repository example.

---
+++ EcoHelp.DataAccess/Repositories/CategoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using EcoHelp.DataAccess.Model;
+
+namespace EcoHelp.DataAccess.Repositories
+{
+    public class CategoryRepository : Repository
+    {
+        /// <summary>
+        /// Method to get all active categories
+        /// </summary>
+        /// <returns></returns>
+        public List<Category> GetTravelRequestsWithPendingApprovals()
+        {
+            List<Category> categories = Db.Categories
+                .Where(c => c.IsActive)
+                .OrderBy(c => c.Name)
+                .ToList();
+
+            return categories;
+        }
+    }
+}
