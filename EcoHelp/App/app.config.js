dec88e5fdd8e70d2f01234a5b692e6934c9f6ec1
Victor De Velasco <develasco.victor@gmail.com>
2017-08-12 13:00:39 -0700

    Initial commit.

---
+++ EcoHelp/App/app.config.js
@@ -0,0 +1,24 @@
+﻿//Routing Configuration
+app.config
+(
+    [
+        "$httpProvider",
+
+        function ($httpProvider)
+        {
+            $httpProvider.defaults.headers.post = {};
+            $httpProvider.defaults.headers.post["Content-Type"] = "application/json; charset=utf-8";
+
+            //initialize get if not there
+            if (!$httpProvider.defaults.headers.get)
+            {
+                $httpProvider.defaults.headers.get = {};
+            }
+
+            //disable IE ajax request caching
+            $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache, no-store, must-revalidate';
+            $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+            $httpProvider.defaults.headers.get['Expires'] = '0';
+        }
+    ]
+);
---
+++ EcoHelp/App/app.module.js
@@ -0,0 +1,15 @@
+﻿// Eco Application Module Creation
+// @module EcoApp
+
+// Create the main module
+var app =
+    angular.module
+    (
+        "EcoApp",
+
+        // Dependency Injection of additional modules
+        [
+            "ngSanitize",
+            "ngAnimate"
+        ]
+    );
