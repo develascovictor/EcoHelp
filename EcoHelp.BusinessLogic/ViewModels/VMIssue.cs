d12590d316e393689d69a8ea2d05d355e6a26626
Victor De Velasco <develasco.victor@gmail.com>
2017-08-20 20:45:54 -0700

    Added new listing option on side nav.

--- EcoHelp.BusinessLogic/ViewModels/VMIssue.cs
+++ EcoHelp.BusinessLogic/ViewModels/VMIssue.cs
@@ -1,14 +1,25 @@
-﻿using EcoHelp.DataAccess.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
+using System.ComponentModel;
+using EcoHelp.DataAccess.Model;
 
 namespace EcoHelp.BusinessLogic.ViewModels
 {
     public class VMIssue
     {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public int PriorityId { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+
+        public VMProperty Priority
+        {
+            get
+            {
+                return ((Priorities)PriorityId).ToVMProperty();
+            }
+        }
+
         public VMIssue()
         {
             EmptyObject();
@@ -16,10 +27,35 @@ namespace EcoHelp.BusinessLogic.ViewModels
 
         public VMIssue(Issue issue)
         {
+            EmptyObject();
+
+            if (issue != null)
+            {
+                Id = issue.IssueId;
+                CategoryId = issue.CategoryId;
+                PriorityId = issue.PriorityId;
+                Name = issue.Name;
+                IsActive = issue.IsActive;
+            }
         }
 
         private void EmptyObject()
         {
+            Id = 0;
+            CategoryId = 0;
+            PriorityId = 0;
+            Name = String.Empty;
+            IsActive = false;
         }
+    }
+
+    public enum Priorities
+    {
+        [Description("Bajo")]
+        Low = 1,
+        [Description("Medio")]
+        Medium = 2,
+        [Description("Alto")]
+        High = 3
     }
 }
--- EcoHelp/App/Components/Home/Home.Controller.js
+++ EcoHelp/App/Components/Home/Home.Controller.js
@@ -2,14 +2,31 @@
 app.controller("Home", Home);
 
 // Injecting modules the controller is dependant of
-Home.$inject = ["$scope"];
+Home.$inject = ["$scope", "HomeService"];
 
-function Home($scope) {
+function Home($scope, HomeService) {
     $scope.CurrentUser = {};
     $scope.FallBackPicture = path + "Resources/images/circle_person.png";
 
+    $scope.Categories = [];
+
     function Init() {
         $scope.CurrentUser.FullName = "Victor De Velasco";
+
+        var promise = HomeService.GetCategories();
+
+        promise.then
+        (
+            function (response) {
+                $scope.Categories = response.data;
+            }
+        )
+        .catch
+        (
+            function (response) {
+                alert('SOMETHING WENT WRONG!')
+            }
+        );
     }
 
     Init();
